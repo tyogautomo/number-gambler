46084fc1a0064c27f57e9c851442fd488b09dbdb
Try Yoga Utomo <47817589+tyogautomo@users.noreply.github.com>
2019-03-31 14:45:20 +0700

    nambah style dan folder img

--- tebakangka.js
+++ tebakangka.js
@@ -1,25 +1,14 @@
-alert("Welcome to \"Number Gambler\" game!!")
+alert("Welcome to \"Guess the Number\" game!!")
 
-var playGame = true;
-var n = 5;
+var gameOn = true;
+var n = 3;
+var compNumber;
 
-while (playGame) {
-    alert("You have " + n + " chance(s) remaining.");
-
-    // USER CHOICE
-    var ngisiAngka = true;
-    while (ngisiAngka) {
-        var userNumber = (prompt("Guess the number from 1 to 10:"));
-        if (userNumber > 10 || userNumber < 1) {
-            alert("Number must be from 1 to 10");
-        } else {
-            ngisiAngka = false;
-        }
-    }
+while (gameOn) {
+    var playGame = true;
 
     // COMPUTER CHOICE
     var random = Math.floor(Math.random() * 10);
-
     switch (random) {
         case 0:
             random = 1;
@@ -53,25 +42,53 @@ while (playGame) {
             break;
     }
 
-    var compNumber = random;
+    compNumber = random;
 
-    // CONDITION
-    if (compNumber == userNumber) {
-        playGame = confirm("Computer choose: " + compNumber + "\nYOU GUESS THE RIGHT NUMBER!! \nWant to try more?");
-    } else if (userNumber < compNumber) {
-        playGame = confirm("Computer choose: " + compNumber + "\nThe number you guessed is to low :( \nWant to try more?");
-    } else {
-        playGame = confirm("Computer choose: " + compNumber + "\nThe number you guessed is to high :( \nWant to try more?");
-    }
+    while (playGame) {
+        alert("You have " + n + " chance(s) remaining.");
 
-    if (userNumber != compNumber) {
-        n--
-    } else {
-        n = 5;
-    }
-    if (n == 0) {
-        playGame = confirm("GAME OVER!!!! Play again??")
-        n = 5
+        // USER CHOICE
+        var ngisiAngka = true;
+        while (ngisiAngka) {
+            var userNumber = (prompt("Guess the number from 1 to 10:"));
+            if (userNumber == null) {
+                ngisiAngka = false;
+                playGame = false;
+                gameOn = false;
+            } else if (userNumber > 10 || userNumber < 1) {
+                alert("Number must be from 1 to 10");
+            } else {
+                ngisiAngka = false;
+            }
+        }
+
+        // CONDITION
+        if (compNumber == userNumber) {
+            gameOn = confirm("Computer choose: " + compNumber + "\nYOU GUESS THE RIGHT NUMBER!! \nWant to try more?");
+            playGame = false;
+        } else if (userNumber == null) {
+            ngisiAngka = false;
+            playGame = false;
+            gameOn = false;
+        } else if (userNumber < compNumber) {
+            alert("\nThe number you guessed is to low :( \nTry again!");
+        } else {
+            alert("\nThe number you guessed is to high :( \nTry again!");
+        }
+
+        //  DECREMENT AND CHANCE RESET
+        if (userNumber != compNumber) {
+            n--
+        } else {
+            n = 3;
+        }
+
+        //  GAME OVER CONDITION
+        if (n == 0) {
+            gameOn = confirm("GAME OVER!!!! \nThe Number is " + compNumber + ". Play again??")
+            playGame = false;
+            n = 3
+        }
     }
 }
 
